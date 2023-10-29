b128be1e21bc16913eb57ade62579000a4efb300
AndrewAday <andrewladay@gmail.com>
2023-10-29 00:25:39 -0700

    switch mesh to use matID and geoID instead of pointers; seemingly fixed twighlight zone segfault?

--- src/renderer/scenegraph/Mesh.cpp
+++ src/renderer/scenegraph/Mesh.cpp
@@ -8,59 +8,46 @@
 
 Mesh::~Mesh()
 {
-    // TODO: only do refcounting if we're the chuck-side copy.
-    // this is because release and add_ref are not thread-safe
-    // decrement any resources we're holding onto
+    CHUGL_RELEASE(GetMaterial());
+    CHUGL_RELEASE(GetGeometry());
+}
 
-    CHUGL_RELEASE(m_Material);
-    // if (m_Material) {
-    //     CKAPI()->object->release( m_Material->m_ChuckObject );
-    // }
+Geometry *Mesh::GetGeometry()
+{
+    return (Geometry*) Locator::GetNode(m_GeoID, IsAudioThreadObject());
+}
 
-    CHUGL_RELEASE(m_Geometry);
-    // if (m_Geometry) {
-    //     CKAPI()->object->release( m_Geometry->m_ChuckObject );
-    // }
+Material *Mesh::GetMaterial()
+{
+    return (Material*) Locator::GetNode(m_MatID, IsAudioThreadObject());
 }
 
 void Mesh::SetGeometry(Geometry *geo)
 {
     // mesh has a pointer to geo, so bump geo refcount
     CHUGL_ADD_REF(geo);
-    // if (geo) {
-    //     CKAPI()->object->add_ref( geo->m_ChuckObject );
-    // }
 
     // remove reference to previous geometry 
     // important: this should happen after add_ref in case
     // the incoming geometry is the same as the current one
 
-    CHUGL_RELEASE(m_Geometry);
-    // if (m_Geometry) {
-    //     CKAPI()->object->release( m_Geometry->m_ChuckObject );
-    // }
+    CHUGL_RELEASE(GetGeometry());
 
-    m_Geometry = geo;
+    m_GeoID = geo ? geo->GetID() : 0;
 }
 
 void Mesh::SetMaterial(Material *mat)
 {
     // mesh has a pointer to material, so bump material refcount
     CHUGL_ADD_REF(mat);
-    // if (mat) {
-    //     CKAPI()->object->add_ref( mat->m_ChuckObject );
-    // }
 
     // remove reference to previous material
     // important: this should happen after add_ref in case
     // the incoming material is the same as the current one
-    CHUGL_RELEASE(m_Material);
-    // if (m_Material) {
-    //     CKAPI()->object->release( m_Material->m_ChuckObject );
-    // }
+    CHUGL_RELEASE(GetMaterial());
 
-    m_Material = mat;
+    m_MatID = mat ? mat->GetID() : 0;
 }
 
-size_t Mesh::GetMaterialID() { return m_Material ? m_Material->GetID() : 0; }
-size_t Mesh::GetGeometryID() { return m_Geometry ? m_Geometry->GetID() : 0; }
+size_t Mesh::GetMaterialID() { return m_MatID; }
+size_t Mesh::GetGeometryID() { return m_GeoID; }
