05c9ccb44f6de1af4d9b184c45339c2c891524fa
AndrewAday <andrewladay@gmail.com>
2024-10-29 21:10:50 -0700

    bump chugl version to 0.2.3

--- src/ChuGL.cpp
+++ src/ChuGL.cpp
@@ -58,7 +58,7 @@
 // clang-format on
 
 // ChuGL version string
-#define CHUGL_VERSION_STRING "0.2.2 (alpha)"
+#define CHUGL_VERSION_STRING "0.2.3 (alpha)"
 
 // metadata required for scene rendering
 struct GG_Config {
